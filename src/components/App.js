ca503a62984f0137e05d6c62afc01b2d2a98a0b3
Omkar Tapale <omkartapale@gmail.com>
2022-06-05 18:49:54 +0530

    Updated Code - Added button to toggle header dark and light mode - Connected state handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,64 @@
-import React, { Component } from 'react'
-import Header from './Bootstrap/Header';
+import React, { Component } from "react";
+import Header from "./Bootstrap/Header";
 
 class App extends Component {
+  state = {
+    Dark: false,
+    Name: "",
+  };
+  toggleHeader = () => {
+    this.setState({
+      Dark: !this.state.Dark,
+    });
+  };
+  changeHandler = (e) => {
+    this.setState({
+      Name: e.target.value,
+    });
+  };
+
   render() {
     return (
-      <div className='App'>
-        <Header>Portfolio Generator</Header>
+      <div className="App">
+        <Header dark={this.state.Dark} className="mb-4">
+          Portfolio Generator
+        </Header>
+        <div className="container">
+          <div className="row">
+            <div className="mb-4 col-12 text-center">
+              <input
+                type="text"
+                name=""
+                id=""
+                className="form-control"
+                value={this.state.Name}
+                onChange={this.changeHandler}
+              />
+            </div>
+            <div className="mb-4 col-12 text-center">
+              <input
+                type="text"
+                name=""
+                id=""
+                className="form-control"
+                value={this.state.Name}
+                onChange={this.changeHandler}
+              />
+            </div>
+            <div className="col-12 text-center">
+              <button
+                className={`btn btn${
+                  this.state.Dark ? "-outline" : ""
+                }-primary`}
+                onClick={this.toggleHeader}
+              >
+                Toggle Dark Mode
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
