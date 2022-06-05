b4e8f7f98f6a2731c3d67783213aa3406e7f9326
Omkar Tapale <omkartapale@gmail.com>
2022-06-05 23:00:45 +0530

    Created the Form elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,46 @@
 import FormGroup from "./Bootstrap/FormGroup";
 
-const Form = ({ FormData }) => {
+const Form = ({ FormData, onChange }) => {
+  const Desc = {
+    FullName: ["text", "Full Name", "This is your full name."],
+    FirstName: [
+      "text",
+      "First Name",
+      "Please enter your first name or given name.",
+    ],
+    LastName: ["text", "Last Name", "Please enter your last name or surname."],
+    Thumbnail: [
+      "text",
+      "Your Photo",
+      "Please upload your photo in a CDN (Ex. Imgur) and paste the Direct Image URL here.",
+    ],
+    URL: [
+      "text",
+      "Website Link",
+      "Please enter the URL of website, where this page is going to be hosted.",
+    ],
+    Description: ["text", "About You", "Write something nice about you."],
+    KeyWords: [
+      "text",
+      "Keywords",
+      "If someone wants to search for you, what keywords they should use.",
+    ],
+    Address: ["text", "Where are you?", "Type in your full address."],
+    Phone: ["tel", "Phone Number", "Please enter your phone number."],
+    Email: ["email", "Email Address", "Tell us your primary email"],
+  };
   return (
     <div className="Form">
-      {Object.keys(FormData).map(
-        (id) => <FormGroup Id={id} Label={id}/>
-      )}
+      {Object.keys(FormData).map((fd) => (
+        <FormGroup
+          key={fd}
+          Label={Desc[fd][1]}
+          Type={Desc[fd][0]}
+          Desc={Desc[fd][2]}
+          ID={fd}
+          onChange={onChange}
+        />
+      ))}
     </div>
   );
 };
